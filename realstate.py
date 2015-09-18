c2e4142fdadbf0354b195d236dda9d3c3eb987cc
carlosdiaz <alberto.carlos@gmail.com>
2015-09-18 08:46:59 -0500

    Changes in app

--- realstate.py
+++ realstate.py
@@ -1,5 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request, session, flash, g
+from functools import wraps
+import sqlite3
+
+
 app = Flask(__name__)
+
+app.database = "marciarealstate.db"
+app.secret_key = "xcFtjs3Ji896Ghm"
+
 
 
 @app.route('/')
@@ -16,12 +24,30 @@ def departamentos():
 
 @app.route('/depaslist')
 def depaslist():
-	return render_template('departamentoslist.html')
+	g.db = connect_db()
+	cur = g.db.execute('select price, type, contract, location, description from properties')
+	propertyRecords = [dict(price=row[0], type=row[1], contract=row[2] , location=row[3], description=row[4]) for row in cur.fetchall()]
+	#(ID TEXT, PRICE TEXT, TYPE TEXT, CONTRACT TEXT, LOCATION TEXT, BATHROOMS TEXT, BEDROOMS TEXT, AREA TEXT, FEATURES TEXT, DESCRIPTION TEXT)""")
+	g.db.close()
+	#return render_template('welcome.html', shoes = shoes)
+	return render_template('departamentoslist.html', propertyRecords = propertyRecords)
 
 @app.route('/casaslist')
 def casaslist():
 	return render_template('casaslist.html')
 
+@app.route('/terrenoslist')
+def terrenoslist():
+	return render_template('terrenoslist.html')
+
+@app.route('/acerca')
+def acerca():
+	return render_template('acerca.html')
+
+def connect_db():
+	return sqlite3.connect(app.database)
+
 
 if __name__ == '__main__':
-    app.run()
+    #app.run()
+    app.run(debug=True)

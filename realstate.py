5aaa4772293372618a1f288d83493c2dd60e60f4
carlosdiaz <alberto.carlos@gmail.com>
2015-10-20 12:52:46 -0500

    Adding changes for main app

--- realstate.py
+++ realstate.py
@@ -16,8 +16,16 @@ def home():
 	if request.method == 'POST':
 		#flash('Es un post' )
 		if request.form['state'] != "" :	
-			valorState = request.form['state']
-			flash('Valor de state %s' %valorState)
+			stateValue = request.form['state']
+			cityValue = request.form['city']
+			statusValue = request.form['status']
+			typeValue = request.form['type']
+			#self.searchProperties(stateValue, cityValue, statusValue, typeValue)
+
+			flash('Valor de state %s ' %stateValue )
+			flash('Valor de city %s '  %cityValue)
+			flash('Valor de city %s '  %statusValue)
+			flash('Valor de city %s '  %typeValue)
 			return render_template('depas.html')
 		else:
 			
@@ -39,16 +47,13 @@ def depaslist():
 	g.db = connect_db()
 	cur = g.db.execute('select price, type, contract, location, description, imagetodisplay from properties')
 	propertyRecords = [dict(price=row[0], type=row[1], contract=row[2] , location=row[3], description=row[4], imagetodisplay=row[5]) for row in cur.fetchall()]
-	#(ID TEXT, PRICE TEXT, TYPE TEXT, CONTRACT TEXT, LOCATION TEXT, BATHROOMS TEXT, BEDROOMS TEXT, AREA TEXT, FEATURES TEXT, DESCRIPTION TEXT)""")
-	
+	#(ID TEXT, PRICE TEXT, TYPE TEXT, CONTRACT TEXT, LOCATION TEXT, BATHROOMS TEXT, BEDROOMS TEXT, AREA TEXT, FEATURES TEXT, DESCRIPTION TEXT)""")	
 	#curLinks = g.db.execute('select name from linksproperties where id = "1"')
 	#linksPropertiesRecords = curLinks.fetchone()
-
 	g.db.close()	
 	#return render_template('departamentoslist.html', propertyRecords = propertyRecords, linksPropertiesRecords = linksPropertiesRecords)
 	return render_template('departamentoslist.html', propertyRecords = propertyRecords)
 	#return render_template('departamentoslist.html',  linksPropertiesRecords = linksPropertiesRecords)
-
 
 @app.route('/casaslist')
 def casaslist():
@@ -65,6 +70,13 @@ def acerca():
 def connect_db():
 	return sqlite3.connect(app.database)
 
+def searchProperties(self, state, city, status, typeprop):
+	g.db = connect_db()
+	cur = g.db.execute('select price, type, contract, location, description, imagetodisplay from properties where state = %s "' %state + '" and type = %s "' %typeprop +'"')
+	#cur = g.db.execute('select price, type, contract, location, description, imagetodisplay from properties where state = %s ' %state  )
+	propertyRecords = [dict(price=row[0], type=row[1], contract=row[2] , location=row[3], description=row[4], imagetodisplay=row[5]) for row in cur.fetchall()]
+	g.db.close()		
+	return propertyRecords
 
 if __name__ == '__main__':
     #app.run()
